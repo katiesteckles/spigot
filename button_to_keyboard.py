1ef3adf942c06e1b7ce231db91224f61e6c2904a
aPaulTaylor <paul.a.taylor@gmail.com>
2022-05-07 19:16:19 +0100

    Create button_to_keyboard.py

---
+++ button_to_keyboard.py
@@ -0,0 +1,13 @@
+import gpio
+import keyboard
+
+button = gpio.Button(9)
+button_state=False
+
+while True:
+    if button.is_active and not button_state:
+        keyboard.press_and_release('a')
+        button_state=True
+    if button_state and not button.is_active:
+        keyboard.press_and_release('b')
+        button_state=False
